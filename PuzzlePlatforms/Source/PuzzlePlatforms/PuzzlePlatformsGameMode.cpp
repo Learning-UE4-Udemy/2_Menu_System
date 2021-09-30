16e5fca04b18da4f284de6355b46c67878ecc774
Aleksandr <brovarsv@gmail.com>
2021-09-30 11:57:03 +0300

    27. Load And Display UMG In C++

--- PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameMode.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameMode.cpp
@@ -4,12 +4,11 @@
 #include "PuzzlePlatformsCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
-APuzzlePlatformsGameMode::APuzzlePlatformsGameMode()
-{
+APuzzlePlatformsGameMode::APuzzlePlatformsGameMode() {
+
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
+	if (PlayerPawnBPClass.Class != NULL) {
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
